4382749959d07010cc6cb1e6007750a1a654919b
Aryan Yadav <142174991+Aryan-Yadav225@users.noreply.github.com>
2024-09-11 17:18:16 +0000

    variable+dataTypes

---
+++ 01_basics/02_variable+dataTypes.js
@@ -0,0 +1,32 @@
+// "use strict"; // treat all JS code as newer version
+
+const accountId = 46738;
+let accountName = "aryanYadav"
+var password = "123ABC";
+cityName = "Pune";
+
+// Use const & let 
+// VAR is risky
+console.table([accountId, accountName, password, cityName]);
+
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+let name = "aryan"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+console.log(typeof age); // number
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
